a4000ccfbebec0822122fbcc2b0024c0d6c9b8e9
n-zuliani <zuliani.nicolo@iisgalvanimi.edu.it>
2020-05-05 09:53:27 +0000

    implemntazione API Google - Zuliani-Gottardo

---
+++ server/routes/speech.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  let text = main(res).catch(console.error);
+});
+
+async function main(res) {
+  // Imports the Google Cloud client library
+  const speech = require('@google-cloud/speech');
+  const fs = require('fs');
+
+  // Creates a client
+  const client = new speech.SpeechClient();
+
+  // The name of the audio file to transcribe
+  const fileName = path.join(__dirname, 'resources/audio.wav');
+
+  // Reads a local audio file and converts it to base64
+  const file = fs.readFileSync(fileName);
+  const audioBytes = file.toString('base64');
+
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 44100,
+    languageCode: 'it-IT',
+    audioChannelCount: 1
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  // Detects speech in the audio file
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+  res.send({text: transcription});
+}
+
+module.exports = router;
