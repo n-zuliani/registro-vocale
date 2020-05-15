b0c2c386b98dcaea85ab8793d1d1981726b70fa1
Joshua Gottardo <gottardo.joshua@iisgalvanimi.edu.it>
2020-05-15 15:08:48 +0000

    sistemata route di inserimento - Gottardo

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,10 +9,7 @@ const config = {
 };
 var express = require('express');
 var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var dateObject = new Date();
 
 router.post('/inserimentoVoto', function (req, res, next) {
   let valutazione = req.body;
@@ -21,7 +18,11 @@ router.post('/inserimentoVoto', function (req, res, next) {
   }
   sql.connect(config, err => {
     let sqlRequest = new sql.Request();
-    let sqlInsert = //query di inserimento
+    let date = ("0" + dateObject.getDate()).slice(-2);
+    let month = ("0" + (dateObject.getMonth() + 1)).slice(-2);
+    let year = dateObject.getFullYear();
+    let full_date = year + "-" + month + "-" + date;
+    let sqlInsert = `INSERT INTO Registro(Voto, Tipo, Data, IDStudente) VALUES('${valutazione.voto}', '${valutazione.tipo}','${full_date}' (SELECT ID FROM Studente WHERE Nome ='${valutazione.nome}' AND Cognome='${valutazione.cognome}'))`;
     sqlRequest.query(sqlInsert, (error, results) => {
         if (error) throw error;
         return res.send({ success: true, message: 'Valutazione inserita con successo', data: results });
