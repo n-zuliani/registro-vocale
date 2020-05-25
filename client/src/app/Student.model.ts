df2a6fa4462a14e6141bf889184105106932eddf
sblendo00 <chri.sble@libero.it>
2020-05-25 14:45:59 +0000

    manca HTML- Sblendorio

---
+++ client/src/app/Student.model.ts
@@ -0,0 +1,4 @@
+export class Student
+{
+  constructor(public Nome: string, public Cognome: string, public Anno: number){};
+}
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Student } from './Student.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  obs: Observable<Student[]>;
+  list: Student[];
+
+  obsUnit: Observable<Student[]>; //L’observable che sta in attesa dei dati
+  data: Student[];
+
+  constructor(private http: HttpClient) { }
+
+  getStudentList(): void {
+    let headers =  {headers: new HttpHeaders().set('Content-Type', 'application/json')};
+    this.obsUnit = this.http.get<Student[]>('https://3000-e3d7f01a-e8d4-401d-a916-b527b20ac4fb.ws-eu01.gitpod.io/student/all', headers);
+    //Mi sottoscrivo all’observable e scrivo la arrow function che riceve i dati
+    this.obsUnit.subscribe((list: Student[]) => {this.list = list;});
+  }
+
+ ngOnInit() {
+    this.obs = this.http.get<Student[]>('https://3000-e3d7f01a-e8d4-401d-a916-b527b20ac4fb.ws-eu01.gitpod.io/student/all');
+    this.obs.subscribe((list: Student[]) => {this.list = list;});
+  }
 }
